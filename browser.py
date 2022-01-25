a159c5fbd344a5195caaf6dd617fc96247045530
Christian Stroh <christian.stroh@washpost.com>
2022-01-25 18:05:39 -0500

    basic browser

---
+++ browser.py
@@ -0,0 +1,48 @@
+import socket
+import urllib.parse
+
+def request(url):
+    assert url.startswith("http://")
+    url = url[len("http://"):]
+    host, path = url.split("/", 1)
+    path = "/" + path
+    s = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP,
+    )
+    s.connect((host, 80))
+    s.send(bytes("GET {} HTTP/1.0\r\n".format(path), encoding="utf8") +
+           bytes("Host: {}\r\n\r\n".format(host), encoding="utf8"))
+    response = s.makefile("r", encoding="utf8", newline="\r\n")
+    statusline = response.readline()
+    version, status, explanation = statusline.split(" ", 2)
+    assert status == "200", "{}: {}".format(status, explanation)
+    headers = {}
+    while True:
+        line = response.readline()
+        if line == "\r\n": break
+        header, value = line.split(":", 1)
+        headers[header.lower()] = value.strip()
+    body = response.read()
+    s.close()
+    return headers, body
+
+def show(body):
+    in_angle = False
+    for c in body:
+        if c == "<":
+            in_angle = True
+        elif c == ">":
+            in_angle = False
+        elif not in_angle:
+            print(c, end="")
+
+def load(url):
+    headers, body = request(url)
+    print(headers, body)
+    show(body)
+
+if __name__ == "__main__":
+    import sys
+    load(sys.argv[1])
