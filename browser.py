9ecdfd5e3df566d7927575af21fdfb0d96b39bad
Christian Stroh <christian.stroh@washpost.com>
2022-01-26 17:36:43 -0500

    http/1.1

--- browser.py
+++ browser.py
@@ -2,17 +2,25 @@ import socket
 import ssl
 import sys
 import urllib.parse
-    
+
+def addHeaders(headers):
+    headers.append("\r\n")
+    joined = "\r\n".join(headers)
+    print("joined", joined)
+    return bytes(joined, encoding="utf8")
+
 def request(url):
     scheme, url = url.split("://", 1)
     assert scheme in ["http", "https"], \
         "Unknown scheme {}".format(scheme)
     host, path = url.split("/", 1)
+
     if ":" in host:
         host, port = host.split(":", 1)
         port = int(port)
     else:
         port = 80 if scheme == "http" else 443
+
     path = "/" + path
     s = socket.socket(
         family=socket.AF_INET,
@@ -20,21 +28,29 @@ def request(url):
         proto=socket.IPPROTO_TCP,
     )
     s.connect((host, port))
+
     if scheme == "https":
         ctx = ssl.create_default_context()
         s = ctx.wrap_socket(s, server_hostname=host)
-    s.send(bytes("GET {} HTTP/1.0\r\n".format(path), encoding="utf8") +
-           bytes("Host: {}\r\n\r\n".format(host), encoding="utf8"))
+
+    s.send(addHeaders([
+        "GET {} HTTP/1.1".format(path),
+        "Host: {}".format(host),
+        "Connection: close",
+        "User-Agent: Weeb/0.0"
+    ]))
     response = s.makefile("r", encoding="utf8", newline="\r\n")
     statusline = response.readline()
     version, status, explanation = statusline.split(" ", 2)
     assert status == "200", "{}: {}".format(status, explanation)
     headers = {}
+    
     while True:
         line = response.readline()
         if line == "\r\n": break
         header, value = line.split(":", 1)
         headers[header.lower()] = value.strip()
+
     body = response.read()
     s.close()
     return headers, body
