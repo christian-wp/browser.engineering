231d51afa26a537b0f36d13aea89e88db222705f
Christian Stroh <christian.stroh@washpost.com>
2022-12-28 14:45:36 -0700

    test.py skeleton

---
+++ test.py
@@ -0,0 +1,30 @@
+import unittest
+
+class TestDownloadingWebPages(unittest.TestCase):
+
+    def test_http11(self):
+        pass
+
+    def test_file_urls(self):
+        pass
+
+    def test_data(self):
+        pass
+
+    def test_body_tag(self):
+        pass
+
+    def test_entities(self):
+        pass
+
+    def test_view_source(self):
+        pass
+
+    def test_compression(self):
+        pass
+
+    def test_redirects(self):
+        pass
+
+    def test_caching(self):
+        pass
